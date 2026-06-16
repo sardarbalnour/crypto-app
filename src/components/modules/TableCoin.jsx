7dd755256e74f4da468f8d136fbee727bad397a9
Sardar balnour <balnour.sardar@gmail.com>
2026-06-16 17:46:08 +0330

    install react loader spinner library and adding loading

--- src/components/modules/TableCoin.jsx
+++ src/components/modules/TableCoin.jsx
@@ -1,27 +1,39 @@
+import { LineWave } from "react-loader-spinner";
+
 import chartUp from "../../assets/chart-up.svg";
 import chartDown from "../../assets/chart-down.svg";
 
-function TableCoin({ coins }) {
+function TableCoin({ coins, isLoading }) {
   console.log(coins);
   return (
     <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Coin</th>
-            <th>Name</th>
-            <th>Price</th>
-            <th>24h</th>
-            <th>Total Volume</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {coins.map((coin) => (
-            <TableRow key={coin.id} coin={coin} />
-          ))}
-        </tbody>
-      </table>
+      {isLoading ? (
+        <LineWave
+          height="300"
+          width="300"
+          firstLineColor="green"
+          middleLineColor="red"
+          lastLineColor="green"
+        />
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>Coin</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>24h</th>
+              <th>Total Volume</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {coins.map((coin) => (
+              <TableRow key={coin.id} coin={coin} />
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
