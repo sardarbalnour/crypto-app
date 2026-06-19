35a5c454c904ec01b30c9aa7aa587250eec25d1e
Sardar balnour <balnour.sardar@gmail.com>
2026-06-19 15:17:22 +0330

    search

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,8 +1,10 @@
 const BASE_URL = "https://api.coingecko.com/api/v3";
 const API_KEY = "CG-hk4aK834L9KCSmcwfMiWdF7H";
 
-const getCoinList = (page, currency) => {
-  return `${BASE_URL}/coins/markets?vs_currency=${currency}&order=market-cap-desc&per_page=20&page=${page}&x_cg_demo_api_key=${API_KEY}`;
-};
+const getCoinList = (page, currency) =>
+  `${BASE_URL}/coins/markets?vs_currency=${currency}&order=market-cap-desc&per_page=20&page=${page}&x_cg_demo_api_key=${API_KEY}`;
 
-export { getCoinList };
+const searchCoin = (query) =>
+  `${BASE_URL}/search?query=${query}&x_cg_demo_api_key=${API_KEY}`;
+
+export { getCoinList, searchCoin };
