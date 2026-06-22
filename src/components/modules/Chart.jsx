0d0591902aacc6bd5737288aee53741095d91546
Sardar balnour <balnour.sardar@gmail.com>
2026-06-22 18:04:14 +0330

    ChartComponent

--- src/components/modules/Chart.jsx
+++ src/components/modules/Chart.jsx
@@ -25,21 +25,7 @@ function Chart({ chart, setChart }) {
       </span>
       <div className={styles.chart}>
         <div className={styles.graph}>
-          <ResponsiveContainer width="100%" height="100%">
-            <LineChart width={400} height={400} data={convertData(chart, type)}>
-              <Line
-                type="monotone"
-                dataKey={type}
-                stroke="#3874ff"
-                strokeWidth="2px"
-              />
-              <CartesianGrid stroke="#404042" />
-              <YAxis dataKey={type} domain={["auto", "auto"]} />
-              <XAxis dataKey="date" hide />
-              <Legend />
-              <Tooltip />
-            </LineChart>
-          </ResponsiveContainer>
+          <ChartComponent data={convertData(chart, type)} type={type} />
         </div>
       </div>
     </div>
@@ -47,3 +33,23 @@ function Chart({ chart, setChart }) {
 }
 
 export default Chart;
+
+const ChartComponent = ({ data, type }) => {
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <LineChart width={400} height={400} data={data}>
+        <Line
+          type="monotone"
+          dataKey={type}
+          stroke="#3874ff"
+          strokeWidth="2px"
+        />
+        <CartesianGrid stroke="#404042" />
+        <YAxis dataKey={type} domain={["auto", "auto"]} />
+        <XAxis dataKey="date" hide />
+        <Legend />
+        <Tooltip />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+};
