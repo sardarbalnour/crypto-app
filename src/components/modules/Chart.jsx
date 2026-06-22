223084af2232d0838540937407e7b8bc9ee79ff2
Sardar balnour <balnour.sardar@gmail.com>
2026-06-22 11:44:58 +0330

    chnaging data format for chart

--- src/components/modules/Chart.jsx
+++ src/components/modules/Chart.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
+
+import { convertData } from "../../helpers/convertData,js";
+
 import styles from "./Chart.module.css";
 
 function Chart({ chart, setChart }) {
+  const[type, setType] = useState("prices");
+  console.log(convertData(chart,type))
+
   return (
     <div className={styles.container}>
-      <span className={styles.cross} onClick={() => setChart(null)}>X</span>
+      <span className={styles.cross} onClick={() => setChart(null)}>
+        X
+      </span>
       <div className={styles.chart}></div>
     </div>
   );
